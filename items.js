8c4a8e484bd2403c244b06901766eab3126cea5f
binh1212011-spec <binh1212011@gmail.com>
2025-09-22 22:25:39 +0700

    Create items.js

---
+++ items.js
@@ -0,0 +1,24 @@
+// Danh sách item + role ID + effect (effect chỉ để hiển thị)
+module.exports = {
+  "Flaws in the world": {
+    roleId: "1418099134644097204",
+  },
+  "Who stands before God": {
+    roleId: "1418099051320180767",
+  },
+  "The Devourer...": {
+    roleId: "1418099355826651219",
+  },
+  "Spotted the Sol": {
+    roleId: "1418099458360610837",
+  },
+  "The Stigma": {
+    roleId: "1418099064217669733",
+  },
+  "The Zero": {
+    roleId: "1418099264982220940",
+  },
+  "The Unknow": {
+    roleId: "1418099416488738967",
+  }
+};
